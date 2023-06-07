786d56298e7928fbb82d107a877f90ac2b32e434
zarni-ein <filippov@mail.ch>
2023-06-07 11:07:42 +0300

    Updates

---
+++ precache-manifest.9009708aa6e4000fd9807a3758674069.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "78bbdb735a12c831cda6",
+    "url": "/nyt-best-sellers/static/css/main.16dbfa98.chunk.css"
+  },
+  {
+    "revision": "78bbdb735a12c831cda6",
+    "url": "/nyt-best-sellers/static/js/main.78bbdb73.chunk.js"
+  },
+  {
+    "revision": "8654e7e83f475fb519e8",
+    "url": "/nyt-best-sellers/static/js/1.8654e7e8.chunk.js"
+  },
+  {
+    "revision": "2e525101408ba65982e5",
+    "url": "/nyt-best-sellers/static/js/runtime~main.2e525101.js"
+  },
+  {
+    "revision": "73d7090df05efaf3d8599fa896a43fc4",
+    "url": "/nyt-best-sellers/static/media/jack_carr_only_the_dead.73d7090d.jpg"
+  },
+  {
+    "revision": "3b5d28a74c6582d1e1c6112816c40530",
+    "url": "/nyt-best-sellers/static/media/emily_henry_happy_place.3b5d28a7.jpg"
+  },
+  {
+    "revision": "a28fe6f55de4596c2598656c2b67ea7a",
+    "url": "/nyt-best-sellers/static/media/colleen_hoover_it_ends_with_us.a28fe6f5.jpg"
+  },
+  {
+    "revision": "54378a75d55b89b2d4eaccd0a73949df",
+    "url": "/nyt-best-sellers/static/media/colleen_hoover_it_starts_with_us.54378a75.jpg"
+  },
+  {
+    "revision": "30199e982bd97f0d36271bd47016f11f",
+    "url": "/nyt-best-sellers/static/media/bonnie_garmus_lessons_in_chemistry.30199e98.jpg"
+  },
+  {
+    "revision": "cf93bdc269580e9fdf9b4cbdd7eb1b28",
+    "url": "/nyt-best-sellers/static/media/bessel_van_der_kolk.cf93bdc2.jpg"
+  },
+  {
+    "revision": "9c57274ced60ef097c5f35dc788d5bf2",
+    "url": "/nyt-best-sellers/static/media/peter_attia_outlive.9c57274c.jpg"
+  },
+  {
+    "revision": "bb0823b026d01f49b8ea706660e13346",
+    "url": "/nyt-best-sellers/static/media/abraham_verghese_the_covenant.bb0823b0.jpg"
+  },
+  {
+    "revision": "2d60ad3c4342e9e6a713045a7035773c",
+    "url": "/nyt-best-sellers/static/media/david_grann_killers.2d60ad3c.jpg"
+  },
+  {
+    "revision": "73acb2469a2917ec73cc283036aa5981",
+    "url": "/nyt-best-sellers/static/media/david_grann_the_wager.73acb246.jpg"
+  },
+  {
+    "revision": "90ea8244817369a5204c0fa42e5d6d1a",
+    "url": "/nyt-best-sellers/index.html"
+  }
+];
